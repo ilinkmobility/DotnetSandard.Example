323280829c4c31dd739bcda819324a7da36fa465
Ramanan Sambandam <ramanans@ilink-systems.com>
2017-12-03 22:43:02 +0530

    .net standard 2.0 issue fix

--- LoanCalculator.UWP/MainPage.xaml.cs
+++ LoanCalculator.UWP/MainPage.xaml.cs
@@ -1,7 +1,17 @@
-﻿using LoanCalculator.Standard;
-using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -10,104 +20,15 @@ namespace LoanCalculator.UWP
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    public sealed partial class MainPage : Page
     {
-        private string _amount;
-        public string amount
-        {
-            get { return _amount; }
-            set
-            {
-                _amount = value;
-                RaisePropertyChanged("amount");
-            }
-        }
-
-        private string _period;
-        public string period
-        {
-            get { return _period; }
-            set
-            {
-                _period = value;
-                RaisePropertyChanged("period");
-            }
-        }
-
-        private string _rate;
-        public string rate
-        {
-            get { return _rate; }
-            set
-            {
-                _rate = value;
-                RaisePropertyChanged("rate");
-            }
-        }
-
-        private string _emi;
-        public string emi
-        {
-            get { return _emi; }
-            set
-            {
-                _emi = value;
-                RaisePropertyChanged("emi");
-            }
-        }
-
-        private string _interest;
-        public string interest
-        {
-            get { return _interest; }
-            set
-            {
-                _interest = value;
-                RaisePropertyChanged("interest");
-            }
-        }
-
-        private string _Result;
-        public string Result
-        {
-            get { return _Result; }
-            set
-            {
-                _Result = value;
-                RaisePropertyChanged("Result");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void RaisePropertyChanged(string name)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(name));
-            }
-        }
-
         public MainPage()
         {
             this.InitializeComponent();
 
-            DataContext = this;
-            Result = "Collapsed";
-        }
+            DataContext = new MainViewModel();
 
-
-
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            amount = period = rate = emi = interest = "";
-        }
-
-        private void Button_Click_Calculate(object sender, RoutedEventArgs e)
-        {
-            Result = "Visible";
-
-            emi = "Monthly EMI : " + string.Format("{0:0.##}", PersonalLoanCalculator.Instance.GetEMI(int.Parse(amount), int.Parse(period), double.Parse(rate)));
-            interest = "Interest : " + string.Format("{0:0.##}", PersonalLoanCalculator.Instance.GetTotalInterestPaid(int.Parse(amount), int.Parse(period), double.Parse(rate)));
+            //textBlock.Text = "" +  LoanCalculator.Standard.PersonalLoanCalculator.Instance.GetEMI(480000, 5, 11.25);
         }
     }
 }
---
+++ LoanCalculator.UWP/MainViewModel.cs
@@ -0,0 +1,103 @@
+﻿using LoanCalculator.Standard;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace LoanCalculator.UWP
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+            storage = value;
+            this.OnPropertyChaned(propertyName);
+            return true;
+        }
+
+        private void OnPropertyChaned(string propertyName)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        int amount;
+        public int Amount
+        {
+            get { return amount; }
+            set
+            {
+                this.SetProperty(ref this.amount, value);
+            }
+        }
+
+        int period;
+        public int Period
+        {
+            get { return period; }
+            set
+            {
+                this.SetProperty(ref this.period, value);
+            }
+        }
+
+        double rate;
+        public double Rate
+        {
+            get { return rate; }
+            set
+            {
+                this.SetProperty(ref this.rate, value);
+            }
+        }
+
+        string emi;
+        public string EMI
+        {
+            get { return emi; }
+            set
+            {
+                this.SetProperty(ref this.emi, value);
+            }
+        }
+
+        string interest;
+        public string Interest
+        {
+            get { return interest; }
+            set
+            {
+                this.SetProperty(ref this.interest, value);
+            }
+        }
+
+        public ICommand CalculateCommand
+        {
+            get
+            {
+                return new CommandHandler(() => this.Calculate());
+            }
+        }
+
+        public MainViewModel()
+        {
+            EMI = "EMI : ";
+            Interest = "Total paid interest : ";
+        }
+
+        public void Calculate()
+        {
+            EMI = "EMI : " + PersonalLoanCalculator.Instance.GetEMI(Amount, Period, Rate);
+            Interest = "Total paid interest : " + PersonalLoanCalculator.Instance.GetTotalInterestPaid(Amount, Period, Rate);
+        }
+    }
+}
